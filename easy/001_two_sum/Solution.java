d11f491dc557456c61b6bc39e5a43e024658df80
luc <lei.a.shi@hotmail.com>
2019-05-08 19:47:19 +0800

    init

---
+++ easy/001_two_sum/Solution.java
@@ -0,0 +1,49 @@
+// Description
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution 
+{
+//    O(n*n) solution should be abandoned.
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; ++i) {
+//            for (int j = i + 1; j < nums.length; ++j) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; ++i) {
+            final Integer value = map.get(nums[i]);
+            // Trick1: put value before i, because value is always smaller than i.
+            if (value != null) {
+                return new int[] { value, i };
+            }
+            // Trick2: key-value should be delta-index, not index-delta.
+            // The reason is HASHMAP LAW No.1- that the delta is the key information, 
+            // and key information must be the key, not value.
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+}
